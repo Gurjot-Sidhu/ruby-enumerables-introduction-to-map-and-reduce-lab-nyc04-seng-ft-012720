fc6616c1811b7193f5656575387cd57e4da939f0
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 05:35:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -46,7 +46,7 @@ def reduce_to_total(source_array,starting_point)
     total+= source_array[i]
     i+=1
   end 
-  return total + starting_point
+  total + starting_point
 end
 
 def reduce_to_all_true(source_array)
