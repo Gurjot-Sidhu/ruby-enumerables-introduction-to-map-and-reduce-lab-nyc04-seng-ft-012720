f182dd8b701a44f8c40d6edc3604ebab724cd0bb
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 05:26:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,13 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array,starting_point)
-  
+  total= 0
+  new = []
+  while i < source_array.length do
+    new.push(source_array[i] * -1)
+    i+=1
+  end 
+  return new
 end
 
 def reduce_to_all_true(source_array)
