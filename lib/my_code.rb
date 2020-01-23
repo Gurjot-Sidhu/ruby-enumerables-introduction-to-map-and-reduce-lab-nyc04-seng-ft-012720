ae388409f15635b86fb9cf09a15fbcfd3aaffccb
Gurjot Sidhu <gurjotsidhu4@gmail.com>
2020-01-23 05:39:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,8 +52,12 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do
-    if
+    if source_array[i] = false 
+      return false
+    end
+    i+=1
   end   
+  
 end
 
 def reduce_to_any_true(source_array)
